621d39ea4776123b1332414ac74a9c367986f9fd
cky2026 <cao.cky2005@yandex.com>
2026-08-30 04:06:02 +0800

    Add Init class with initialization methods

---
+++ sources/com/github/catvod/spider/Init.java
@@ -0,0 +1,13 @@
+package com.github.catvod.spider;
+
+import android.content.Context;
+
+public class Init {
+
+    public static void init(Context context) {
+        // 无需额外初始化，占位即可
+    }
+
+    public static void init() {
+    }
+}
